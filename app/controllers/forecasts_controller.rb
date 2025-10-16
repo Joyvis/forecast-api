70a5be742b91128453c5f7e260edb01e91e846b4
Joyvis Santana <jjs276@gmail.com>
2025-10-16 15:12:05 -0300

    feat(forecasts): implement query param verification and include debugging lib

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,10 @@
 class ForecastsController < ApplicationController
   # BASE_URL/api/v1/forecasts
   def index
-    render json: {}, status: :ok
+    if params[:zipcode].present? || params[:address].present?
+      render json: {}, status: :ok
+    else
+      render status: :bad_request
+    end
   end
 end
--- spec/requests/forecasts_spec.rb
+++ spec/requests/forecasts_spec.rb
@@ -2,11 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Forecasts", type: :request do
   describe "GET /index" do
-    before { get '/forecasts?zipcode=11999111' }
+    BASE_URL = '/forecasts'.freeze
+
+    before { get url }
 
     context "with valid query params" do
-      it 'renders a json response with http status ok' do
-        expect(response).to have_http_status(:ok)
+      context 'with zipcode' do
+        let(:url) { "#{BASE_URL}?zipcode=11222000" }
+
+        it 'renders a json response with http status ok' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'with address' do
+        let(:url) { "#{BASE_URL}?address=foobar" }
+
+        it 'renders a json response with http status ok' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+
+    context "with invalid query params" do
+      let(:url) { "#{BASE_URL}" }
+
+      it 'renders a json response with http status bad request' do
+        expect(response).to have_http_status(:bad_request)
       end
     end
   end
