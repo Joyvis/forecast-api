67848c75cd872b7a25affd6a894e88cba17c1a20
Joyvis Santana <jjs276@gmail.com>
2025-11-11 15:44:06 -0300

    refact(locations-api-client): break fetch method into smaller ones

--- app/external_api_clients/locations_api_client.rb
+++ app/external_api_clients/locations_api_client.rb
@@ -8,16 +8,8 @@ class LocationsApiClient < BaseApiClient
   def fetch(address:, force: false)
     raise LocationNotFoundError unless address
 
-    encoded_address = CGI.escape(address)
-    response = Rails.cache.fetch("locations/#{normalize_address(address)}", expires_in: 30.minutes, force: force) do
-      get("#{BASE_URL}/search?q=#{encoded_address}&api_key=#{API_KEY}")
-    end
-
-    response.map do |record|
-      Struct
-        .new(:longitude, :latitude, :address, :zipcode)
-        .new(record['lon'], record['lat'], record['display_name'], record['address']['postcode'])
-    end
+    locations = fetch_locations(address:, force:)
+    serialize_location(locations)
   end
 
   def self.fetch(address:, force: false)
@@ -25,6 +17,26 @@ class LocationsApiClient < BaseApiClient
   end
 
   private
+
+  def serialize_location(locations)
+    locations.map do |record|
+      Struct
+        .new(:longitude, :latitude, :address, :zipcode)
+        .new(
+          record['lon'],
+          record['lat'],
+          record['display_name'],
+          record['address']['postcode']
+        )
+    end
+  end
+
+  def fetch_locations(address:, force:)
+    encoded_address = CGI.escape(address)
+    Rails.cache.fetch("locations/#{normalize_address(address)}", expires_in: 30.minutes, force: force) do
+      get("#{BASE_URL}/search?q=#{encoded_address}&api_key=#{API_KEY}")
+    end
+  end
 
   def normalize_address(raw)
     text = raw.to_s.downcase
